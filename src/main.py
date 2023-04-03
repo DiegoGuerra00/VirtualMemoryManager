a147c9632f9ba99273af753330fce55ab5032bf0
DiegoGuerra00 <di.guerra00@gmail.com>
2023-04-03 17:53:12 -0300

    add binary to decimal convertion

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def main():
+    pass
+
+
+# Recebe o binário como string e converte para decimal
+def convertBinToDec(binNumber):
+    return int(binNumber, 2)
+
+
+if __name__ == "__main__":
+    main()
